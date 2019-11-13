f037f9555993af8f472ca27fabe1d7857fb71fc3
01048750 <01048750@NK-LABW14017.ncc.commnet.edu>
2019-11-13 10:40:00 -0500

    Completed step 4

--- src/colorchooser/JColorChooser.java
+++ src/colorchooser/JColorChooser.java
@@ -1,5 +1,9 @@
 package colorchooser;
 
+import colorchoosercomponents.ColorEvent;
+import colorchoosercomponents.ColorListener;
+import java.awt.Color;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,7 +15,8 @@ package colorchooser;
  *
  * @author 00220682
  */
-public class JColorChooser extends javax.swing.JFrame {
+public class JColorChooser extends javax.swing.JFrame implements ColorListener{
+    private Color color;
 
     /**
      * Creates new form JColorChooser
@@ -53,6 +58,7 @@ public class JColorChooser extends javax.swing.JFrame {
         colorTextFieldBlue = new colorchoosercomponents.ColorTextField();
         jLabel8 = new javax.swing.JLabel();
         jLabel9 = new javax.swing.JLabel();
+        btnclose = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("JColorChooser");
@@ -88,6 +94,13 @@ public class JColorChooser extends javax.swing.JFrame {
         jPanel1.add(jLabel8);
         jPanel1.add(jLabel9);
 
+        btnclose.setText("Close");
+        btnclose.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btncloseActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -98,7 +111,10 @@ public class JColorChooser extends javax.swing.JFrame {
                         .addContainerGap()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(canvas, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(label, javax.swing.GroupLayout.DEFAULT_SIZE, 380, Short.MAX_VALUE)))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(label, javax.swing.GroupLayout.DEFAULT_SIZE, 317, Short.MAX_VALUE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(btnclose))))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -115,12 +131,18 @@ public class JColorChooser extends javax.swing.JFrame {
                     .addComponent(chooser, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(label, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(16, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(label, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnclose))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btncloseActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btncloseActionPerformed
+        close();
+    }//GEN-LAST:event_btncloseActionPerformed
 
     /**
      * @param args the command line arguments
@@ -158,6 +180,7 @@ public class JColorChooser extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnclose;
     private colorchoosercomponents.ColorCanvas canvas;
     private colorchoosercomponents.ColorChooser chooser;
     private colorchoosercomponents.ColorTextField colorTextFieldBlue;
@@ -175,4 +198,14 @@ public class JColorChooser extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private colorchoosercomponents.ColorLabel label;
     // End of variables declaration//GEN-END:variables
+
+    @Override
+    public void changeColor(ColorEvent ce) {
+        this.color = ce.getColor();
+        }
+    
+    public Color close(){
+        this.dispose();
+        return this.color;
+    }
 }
--- src/colorchoosercomponents/ColorTextField.java
+++ src/colorchoosercomponents/ColorTextField.java
@@ -30,7 +30,6 @@ public class ColorTextField extends JTextField implements ColorListener{
 
     @Override
     public void changeColor(ColorEvent ce) {
-        System.out.println("Test");
         if (color == "red") setText(Integer.toString(ce.getColor().getRed()));
         else if (color == "green") setText(Integer.toString(ce.getColor().getGreen()));
         else if (color == "blue") setText(Integer.toString(ce.getColor().getBlue()));
