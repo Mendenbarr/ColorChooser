107bfa2b93cea09cfeed2aba7e2fcc1027d58e75
I-J-C <I-J-C@users.noreply.github.com>
2019-11-13 16:42:28 -0500

    Panel pretty much done, just working on connecting it to the sliders

--- src/colorchoosercomponents/ColorTextField.java
+++ src/colorchoosercomponents/ColorTextField.java
@@ -5,13 +5,13 @@
  */
 package colorchoosercomponents;
 
-import javax.swing.JTextField;
+import colorfield.JRegExpField;
 
 /**
  *
  * @author 01048750
  */
-public class ColorTextField extends JTextField implements ColorListener{
+public class ColorTextField extends JRegExpField implements ColorListener{
     private String color;
 
     public String getColor() {
@@ -25,6 +25,7 @@ public class ColorTextField extends JTextField implements ColorListener{
     public ColorTextField() {
         super();
         setText("0");
+        setExp("\\b(1?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\\b");
         
     }
 
@@ -34,5 +35,4 @@ public class ColorTextField extends JTextField implements ColorListener{
         else if (color == "green") setText(Integer.toString(ce.getColor().getGreen()));
         else if (color == "blue") setText(Integer.toString(ce.getColor().getBlue()));
     }
-    
 }
